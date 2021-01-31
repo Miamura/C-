c1115d686184f51e375c1d0dddaf2a7d92cc21cf
Fethi Abalı <42973841+Miamura@users.noreply.github.com>
2021-01-31 12:40:54 +0300

    Add files via upload

---
+++ Designpattern/ConsoleApp3/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Dwarwen
+    {
+        private static Dwarwen Dwarwen_nerede;
+        private int Dwrwn_Attack;
+
+        protected Dwarwen()
+        {
+            Console.WriteLine("Cüceler oluşturuldu");
+        }
+
+        public static Dwarwen GetInstance()
+        {
+            if (Dwarwen_nerede == null)
+            {
+                Dwarwen_nerede = new Dwarwen();
+            }
+            return Dwarwen_nerede;
+        }
+
+
+        public int VeriGir()
+        {
+            return Dwrwn_Attack;
+        }
+
+        public void VeriAl(int val)
+        {
+            Dwrwn_Attack = val;
+        }
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*crate object yerine getınstance vardı kaynaklarda getınstanceyi arattığımda 
+              sadece singleton ile ilgili olduğunu gördüm*/
+
+            Dwarwen Thorin = Dwarwen.GetInstance();
+            Dwarwen Gimli = Dwarwen.GetInstance();
+            Dwarwen Gloin = Dwarwen.GetInstance();
+
+            Thorin.SetData(1500);
+
+            Console.WriteLine("Yeni cüceler oluşturuluyor");
+
+            Console.WriteLine("Thorin verisi {0} \n", Thorin.Verigir());
+            Console.WriteLine("Gimli verisi {0} \n", Gimli.Verigir());
+            Console.WriteLine("Gloin verisi {0} \n", Gloin.Verigir());
+
+            Console.ReadKey();
+            
+            Thorin.SetData(3000);
+
+            Console.WriteLine("Thorin verisi {0} \n", Thorin.Verigir());
+            Console.WriteLine("Gimli verisi {0} \n", Gimli.Verigir());
+            Console.WriteLine("Gloin verisi {0} \n", Gloin.Verigir());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Designpattern/PrototypeMethod/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeMethod
+{
+    public class Veri : ICloneable
+    {
+        private string isim;
+    
+        private ArrayList Nan = null;
+    
+    public string Isim
+        {
+            get
+            {
+                return isim;
+            }
+            set
+            {
+                isim = value;
+            }
+        }
+        public Veri ()
+        {
+            isim = "Maheshavara";
+            string[] Irklarinadları = { "Cüceler", "Elfler", "Goblinler" };
+
+            Nan = new Arraylist();
+            Nan.AddRange(Irklarinadları);
+        }
+        
+        public void Irkekle(string isim)
+        {
+            Nan.Add(isim);
+        }
+
+        public void IrklarınListesi()
+        {
+            for(int i=0;i < Nan.Count-1 ;++i)
+            {
+                Console.WriteLine(Nan[i]);
+            }
+        }
+        
+        public object Clone()
+        {
+            return new Veri();
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Veri cingu = new Veri();
+
+            Veri IL = (Veri)cingu.Clone();
+            IL.Isim = "İnsanlar";
+            IL.Irkekle("Haladrim");
+            Console.WriteLine(IL.Isim);
+            IL.IrklarınListesi();
+            Veri IL2 = (Veri)cingu.Clone();
+            IL2.Isim = "Uruk Hai";
+            IL2.Irkekle("Orclar");
+            Console.WriteLine(IL2.Isim);
+            IL2.IrklarınListesi();
+        }
+    }
+}
